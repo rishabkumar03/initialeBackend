4523f0422f927f2cc159c67e31477f6b0a36bc3f
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-02-22 06:13:21 -0800

    first message

---
+++ 02index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('rishab.verma03')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Fuck Your Feelings</h1>')
+})
+
+app.get('/darkTheme', (req, res) => {
+    res.send('<h3>Is my love worth the wait?</h3>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
